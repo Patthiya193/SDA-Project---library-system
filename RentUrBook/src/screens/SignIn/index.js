64654ef8ee0a0073c01344f13159ccab17269393
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-05 21:43:27 +0700

    Update import and remove unused code

--- RentUrBook/src/screens/SignIn/index.js
+++ RentUrBook/src/screens/SignIn/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
-import { View, Dimensions, ScrollView, Text } from "react-native";
+import { View, Dimensions, ScrollView, Text, TextInput } from "react-native";
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faUserCircle, faLock } from '@fortawesome/free-solid-svg-icons';
 
 import { styles } from "./styles";
 
 const SignIn = ({navigation, route}) => {
     return(
-        <View style={styles.body}>
-            <Text>
-                Sign In
-            </Text>
         </View>
+        
     )
 }
 
