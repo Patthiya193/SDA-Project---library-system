4a9880c888f180161ba974d29ff1f1eb1736ec6d
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-05 17:20:28 +0700

    Create Sign in screen

---
+++ RentUrBook/src/screens/SignIn/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+import { View, Dimensions, ScrollView, Text } from "react-native";
+
+import { styles } from "./styles";
+
+const SignIn = ({navigation, route}) => {
+    return(
+        <View style={styles.body}>
+            <Text>
+                Sign In
+            </Text>
+        </View>
+    )
+}
+
+export default SignIn;
