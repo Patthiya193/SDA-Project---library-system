5f9aab117022ec8663077df00077f5923c7bdbbf
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-05 21:44:03 +0700

    Add parts of login page

--- RentUrBook/src/screens/SignIn/index.js
+++ RentUrBook/src/screens/SignIn/index.js
@@ -9,6 +9,23 @@ import { styles } from "./styles";
 
 const SignIn = ({navigation, route}) => {
     return(
+        <View style={styles.background}>
+            <View style={styles.top}>
+                <View style={styles.circleIcon}>
+                    <Text style={styles.iconText}> B </Text>
+                </View>
+            </View>
+            <View style={styles.mainBody}>
+                <Text style={styles.title}> Login to RentUrBook </Text>
+                <View style={styles.inputContainer}>
+                    <FontAwesomeIcon icon={ faUserCircle } color='#A8AFB9' size={24} />
+                    <TextInput placeholder='Username' style={styles.textInput}/>
+                </View>
+                <View style={styles.inputContainer}>
+                    <FontAwesomeIcon icon={ faLock } color='#A8AFB9' size={24} />
+                    <TextInput placeholder='Password' style={styles.textInput}/>
+                </View>
+            </View>
         </View>
         
     )
