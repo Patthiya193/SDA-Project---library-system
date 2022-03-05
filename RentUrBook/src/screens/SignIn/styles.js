d61e14ad562f44b42e43cc1a8612361fdcc1d162
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-05 21:42:20 +0700

    Add styles for each component
    - background: whole page background
    - circleIcon/iconText: custom top page icon
    -  top: top part of the page (purple part)
    - mainBody: main body of the page (white part)
    - title: text heading
    - inputContainer/textInput: input text box

--- RentUrBook/src/screens/SignIn/styles.js
+++ RentUrBook/src/screens/SignIn/styles.js
@@ -1,7 +1,60 @@
-import { StyleSheet } from "react-native";
+
+import { StyleSheet, Dimensions } from "react-native";
 
 export const styles = StyleSheet.create({
-    body: {
-        backgroundColor:'#6C70EB',
+    background: {
+        flex: 1,
+        backgroundColor: '#6C70EB',
+    },
+    circleIcon: {
+        height: Dimensions.get('window').height * 0.1,
+        width: Dimensions.get('window').height * 0.1,
+        borderRadius: Math.round((Dimensions.get('window').height + Dimensions.get('window').width) / 2),
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    iconText: {
+        fontWeight: 'bold',
+        fontSize: 48,
+        color: '#6C70EB',
+    },
+    top: {
+        flex: 1.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    mainBody: {
+        flex: 3,
+        backgroundColor: '#F9FAFB',
+        borderTopLeftRadius: 44,
+        borderTopRightRadius: 44,
+        paddingVertical: 20,
+        paddingHorizontal: 30,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'black',
+        marginTop: 30,
+        marginBottom: 30,
+    },
+    inputContainer: {
+        width: Dimensions.get('window').width * 0.75,
+        height: Dimensions.get('window').width * 0.125,
+        marginTop: 10,
+        marginBottom: 10,
+        flexDirection: 'row',
+        backgroundColor: '#ffffff',
+        borderColor: '#A8AFB9',
+        alignItems: 'center',
+        paddingLeft: 5,
+        borderRadius: 16,
+    },
+    textInput: {
+        color: '#A8AFB9',
+        paddingLeft: 10,
     },
 })
