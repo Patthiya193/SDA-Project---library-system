d39cb33d3884f6665e82f77f5d937d6fb3617f62
Phurit Warapattanapong <phuritwarapat@gmail.com>
2022-03-24 17:51:20 +0700

    library system folder

---
+++ librarysystem/src/test/java/com/renturbook/librarysystem/LibrarysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renturbook.librarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
