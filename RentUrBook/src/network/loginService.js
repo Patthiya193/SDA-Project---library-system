882f5367a6911c74c0f1dcdab59f97875537bbf2
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-27 15:04:33 +0700

    Update get function

--- RentUrBook/src/network/loginService.js
+++ RentUrBook/src/network/loginService.js
@@ -1,9 +1,12 @@
 
 export const getUser = async () => {
+    const userUrl = "http://<yourip>:8080/api/v1/user"; //remove ip everytime you commit for security
+    
     try {
         const response = await fetch(userUrl);
         const json = await response.json();
-        return json.movies;
+        console.log(json);
+        return json;
     } 
     catch (error) {
         console.error(error);
