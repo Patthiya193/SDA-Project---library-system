17122905484d0bbf142837874381b85cfcc7fca5
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-27 15:02:40 +0700

    Remove unused code

--- RentUrBook/src/network/loginService.js
+++ RentUrBook/src/network/loginService.js
@@ -1,9 +1,5 @@
-import { useState } from "react";
-
-const userUrl = "localhost:8080/api/v1/user";
 
 export const getUser = async () => {
-    // const [data, setData] = useState([]);
     try {
         const response = await fetch(userUrl);
         const json = await response.json();
