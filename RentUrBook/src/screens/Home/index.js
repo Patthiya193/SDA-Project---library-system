01b4b1d37ed4b21c09f7393fb67f99dbfcea9720
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-05 16:14:24 +0700

    Add content to home page

--- RentUrBook/src/screens/Home/index.js
+++ RentUrBook/src/screens/Home/index.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faUserCircle, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import { styles } from "./styles";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
@@ -17,7 +17,19 @@ const SignIn = ({navigation, route}) => {
 
     return(
         <View style={styles.background}>
-            
+            <View style={styles.top}>
+                <Text style={styles.title}>Rent Ur Book</Text>
+                <View style={styles.inputContainer}>
+                    <FontAwesomeIcon icon={ faSearch } color='#A8AFB9' size={24}  />
+                    <TextInput placeholder='Search for books' style={styles.textInput} placeholderTextColor='#A8AFB9'/>
+                </View>
+            </View>
+            <View style={styles.mainBody}>
+                
+            </View>
+            <View style={styles.footer}>
+
+            </View>
         </View>
     )
 }
--- RentUrBook/src/screens/Home/styles.js
+++ RentUrBook/src/screens/Home/styles.js
@@ -3,7 +3,49 @@ import { StyleSheet, Dimensions } from "react-native";
 
 export const styles = StyleSheet.create({
     background: {
-        flex: 3,
+        flex: 1,
         backgroundColor: '#6C70EB',
+    },
+    top: {
+        flex: 1.75,
+        alignItems: 'center',
+        paddingTop: "10%",
+    },
+    mainBody: {
+        flex: 3,
+        backgroundColor: '#F9FAFB',
+        borderTopLeftRadius: 44,
+        borderTopRightRadius: 44,
+        paddingVertical: 20,
+        paddingHorizontal: 30,
+        paddingTop: 30,
+        alignItems: 'center',
+    },
+    footer: {
+        
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: 'white',
+        marginTop: 30,
+        marginBottom: 30,
+    },
+    inputContainer: {
+        width: Dimensions.get('window').width * 0.75,
+        height: Dimensions.get('window').width * 0.11,
+        marginTop: 10,
+        marginBottom: 10,
+        flexDirection: 'row',
+        backgroundColor: '#ffffff',
+        borderColor: '#A8AFB9',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingLeft: 10,
+        borderRadius: 16,
+    },
+    textInput: {
+        fontWeight: 'bold',
+        paddingLeft: 5,
     },
 })
