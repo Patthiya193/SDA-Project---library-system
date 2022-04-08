0cf0f02d3bb3d3b56bd7ea3149457a6701794df1
Patthiya193 <79250844+Patthiya193@users.noreply.github.com>
2022-04-08 19:49:25 +0700

    fix bug

--- RentUrBook/src/utils/ipaddr.js
+++ RentUrBook/src/utils/ipaddr.js
@@ -1,2 +1,2 @@
-export const userLoginApi = "http://192.168.0.108:8080/api/v1/user/login/";
-export const getUserApi = "http://192.168.0.108:8080/api/v1/user"
+export const userLoginApi = "http://192.168.0.103:8080/api/v1/user/login/";
+export const getUserApi = "http://192.168.0.103:8080/api/v1/user"
