94384c0ac4c36c42d8112b97363bb0c2fb0ddf6d
Patthiya193 <79250844+Patthiya193@users.noreply.github.com>
2022-04-08 20:12:07 +0700

    Update ipaddr.js

--- RentUrBook/src/utils/ipaddr.js
+++ RentUrBook/src/utils/ipaddr.js
@@ -1,2 +1,5 @@
 export const userLoginApi = "http://192.168.0.103:8080/api/v1/user/login/";
 export const getUserApi = "http://192.168.0.103:8080/api/v1/user"
+export const registerUserApi = "http://192.168.0.103:8080/api/v1/user"
+export const getUserByIdApi = "http://192.168.0.103:8080/api/v1/user/id/"
+export const checkUserNameApi = "http://192.168.0.103:8080/api/v1/user/checkusername/"
