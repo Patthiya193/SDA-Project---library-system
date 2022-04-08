5cd24448604a1448648075d6444ffbce95f8e263
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-08 15:47:25 +0700

    Remove unused styles

--- RentUrBook/src/screens/Home/styles.js
+++ RentUrBook/src/screens/Home/styles.js
@@ -2,33 +2,8 @@
 import { StyleSheet, Dimensions } from "react-native";
 
 export const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        backgroundColor: '#6C70EB',
-    },
-    top: {
-        flex: 1.5,
-        alignItems: 'center',
-        paddingTop: "10%",
-    },
-    mainBody: {
-        flex: 3,
-        backgroundColor: '#f2f2f2',
-        borderTopLeftRadius: 44,
-        borderTopRightRadius: 44,
-        paddingVertical: 20,
-        paddingHorizontal: 30,
-        paddingTop: 30,
-    },
     footer: {
         backgroundColor: 'white',
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        color: 'white',
-        marginTop: 30,
-        marginBottom: 30,
     },
 })
 
@@ -53,20 +28,4 @@ export const tabBarStyles = StyleSheet.create({
         fontSize: 14,
         fontWeight: 'bold',
     },
-})
-
-export const bookItemStyles = StyleSheet.create({
-    bookName: {
-        fontWeight: 'bold',
-        fontSize: 18,
-        margin: 10,
-    },
-    bookAuthor: {
-        fontSize: 14,
-        margin: 10,
-    },
-    bookContainer: {
-        paddingLeft: 0,
-        padding: '10%',
-    }, 
 })
