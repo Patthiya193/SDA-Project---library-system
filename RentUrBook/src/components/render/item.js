b867e87bb7b6cf71ec5564a4afa116e09394fc1d
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-08 15:45:20 +0700

    Move to component folder

---
+++ RentUrBook/src/components/render/item.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+import { bookItemStyles } from "../../screens/universalSyles";
+
+export const Item = ({item, onPress}) => (
+    <TouchableOpacity style={bookItemStyles.bookContainer}>
+        <Text style={bookItemStyles.bookName}>{item.title}</Text>
+        <Text style={bookItemStyles.bookAuthor}>{item.subtitle}</Text>
+    </TouchableOpacity>
+);
