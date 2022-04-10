82c8329c1da87b1b51d9e868fdb8f7e110b2e6e4
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-10 15:29:52 +0700

    Update ip addr file

--- RentUrBook/src/utils/ipaddr.js
+++ RentUrBook/src/utils/ipaddr.js
@@ -1,10 +1,12 @@
-export const userLoginApi = "http://192.168.0.108:8080/api/v1/user/login/";
-export const getUserApi = "http://192.168.0.108:8080/api/v1/user"
-export const registerUserApi = "http://192.168.0.108:8080/api/v1/user"
-export const getUserByIdApi = "http://192.168.0.108:8080/api/v1/user/id/"
-export const checkUserNameApi = "http://192.168.0.108:8080/api/v1/user/checkusername/"
-export const addFavApi = "http://192.168.0.108:8080/api/v1/user/addfav/"
-export const removeFavApi = "http://192.168.0.108:8080/api/v1/user/removefav/"
+const ip = "192.168.1.110"
 
-export const getAllBookApi = "http://192.168.0.108:8080/api/v1/book"
-export const borrowBookApi = "http://192.168.0.108:8080/api/v1/book/borrow/"
+export const userLoginApi = `http://${ip}:8080/api/v1/user/login/`;
+export const getUserApi = `http://${ip}:8080/api/v1/user`;
+export const registerUserApi = `http://${ip}:8080/api/v1/user`;
+export const getUserByIdApi = `http://${ip}:8080/api/v1/user/id/`;
+export const checkUserNameApi = `http://${ip}:8080/api/v1/user/checkusername/`;
+export const addFavApi = `http://${ip}:8080/api/v1/user/addfav/`;
+export const removeFavApi = `http://${ip}:8080/api/v1/user/removefav/`;
+
+export const getAllBookApi = `http://${ip}:8080/api/v1/book`;
+export const borrowBookApi = `http://${ip}:8080/api/v1/book/borrow/`;
