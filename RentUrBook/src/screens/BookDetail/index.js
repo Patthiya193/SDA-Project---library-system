b490eabb828c62b8870721218a0208f3e1442209
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-14 15:53:56 +0700

    Add isbn number to the display

--- RentUrBook/src/screens/BookDetail/index.js
+++ RentUrBook/src/screens/BookDetail/index.js
@@ -123,8 +123,9 @@ const BookDetail = ({navigation, route}) => {
                         {/* Book name */}
                         <Text style = {styles.bookName}>{book["bookName"]}</Text> 
                         {/* Book Author */}
-                        <Text style = {styles.bookAuthor}>{author}</Text> 
-                        
+                        <Text style = {styles.bookDetails}>{author}</Text> 
+                        {/* ISBN number */}
+                        <Text style = {styles.bookDetails}>{isbn}</Text>
                         
                     </View>
                 
@@ -181,7 +182,9 @@ const BookDetail = ({navigation, route}) => {
                         {/* Book name */}
                         <Text style = {styles.bookName}>{book["bookName"]}</Text> 
                         {/* Book Author */}
-                        <Text style = {styles.bookAuthor}>{author}</Text> 
+                        <Text style = {styles.bookDetails}>{author}</Text> 
+                        {/* ISBN number */}
+                        <Text style = {styles.bookDetails}>{isbn}</Text>
                         
                     </View>
                     
@@ -226,9 +229,9 @@ const BookDetail = ({navigation, route}) => {
                     {/* Book name */}
                     <Text style = {styles.bookName}>{book["bookName"]} </Text> 
                     {/* Book Author */}
-                    <Text style = {styles.bookAuthor}> {author} </Text> 
-                
-                
+                    <Text style = {styles.bookDetails}>{author}</Text> 
+                    {/* ISBN number */}
+                    <Text style = {styles.bookDetails}>{isbn}</Text>
                 </View>
             
                 <SafeAreaView style={styles.textContainer}>
