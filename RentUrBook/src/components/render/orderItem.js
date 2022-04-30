26ef53c5c52260c23461d244e45a8c0ce980746b
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-30 17:33:42 +0700

    Update item padding

--- RentUrBook/src/components/render/orderItem.js
+++ RentUrBook/src/components/render/orderItem.js
@@ -10,8 +10,8 @@ export const OrderItem = ({item, onPress}) => {
     const [image, setImage] = useState(null)
 
     useEffect(() => {
-        console.log(item.bookId)
-        getBookById(item.bookId)
+        // console.log(item.bookId)
+        getBookById(item.orderObject.bookId)
         .then(book => setImage(book["coverImage"])
         
     ,[])
@@ -20,7 +20,7 @@ export const OrderItem = ({item, onPress}) => {
     return (
         <TouchableOpacity style={bookItemStyles.bookContainer} onPress = {onPress}>
         { image ? (<Image source={{uri: `data:image/jpeg;base64,${image}`}} style={{width: 100, height: 150, paddingBottom:10}} />) : (<></>)}
-        <View style={{justifyContent:'center'}}>
+        <View style={{justifyContent:'center', paddingLeft: 15}}>
             <Text style={bookItemStyles.bookName}>{item.title}</Text>
             <Text style={bookItemStyles.bookDetails}>Borrowed by: {item.borrowedBy}</Text>
             <Text style={bookItemStyles.bookDetails}>{item.status}</Text>
