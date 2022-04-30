10826bed33af6048a850b92291b8d4ab5a627a7c
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-05-01 01:53:02 +0700

    add right padding

--- RentUrBook/src/components/render/item.js
+++ RentUrBook/src/components/render/item.js
@@ -12,7 +12,7 @@ export const Item = ({item, onPress}) =>{
     return (    
     <TouchableOpacity style={bookItemStyles.bookContainer} onPress = {onPress}>
         <Image source={{uri: `data:image/jpeg;base64,${item.bookObject["coverImage"]}`}} style={{width: 100, height: 150, paddingBottom:10}} />
-        <View style={{justifyContent:'center', paddingLeft: 15}}>
+        <View style={{justifyContent:'center', paddingLeft: 15, paddingRight: 15}}>
             <Text style={bookItemStyles.bookName}>{item.title}</Text>
             <Text style={bookItemStyles.bookDetails}>{item.subtitle}</Text>
             <Text style={bookItemStyles.bookDetails}>isbn: {item.isbn}</Text>
