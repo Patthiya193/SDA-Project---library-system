dbcbaeddcea30b9e8251ed239c68093d3ae01d1f
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-30 16:53:06 +0700

    Add import

--- RentUrBook/src/components/render/orderItem.js
+++ RentUrBook/src/components/render/orderItem.js
@@ -1,13 +1,14 @@
 
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native";
+
+import { TouchableOpacity, Text, View, Image } from "react-native";
 import { BookIcon } from "../../screens/Home/bookIcon";
 
 import { bookItemStyles } from "../../screens/universalStyles";
 
 export const OrderItem = ({item, onPress}) => (
     <TouchableOpacity style={bookItemStyles.bookContainer} onPress = {onPress}>
-        <Image source={{uri: `data:image/jpeg;base64,${item.bookObject["coverImage"]}`}} style={{width: 100, height: 150, paddingBottom:10}} />
+        {/* <Image source={{uri: `data:image/jpeg;base64,${item.bookObject["coverImage"]}`}} style={{width: 100, height: 150, paddingBottom:10}} /> */}
         <View style={{justifyContent:'center'}}>
             <Text style={bookItemStyles.bookName}>{item.title}</Text>
             <Text style={bookItemStyles.bookDetails}>Borrowed by: {item.borrowedBy}</Text>
