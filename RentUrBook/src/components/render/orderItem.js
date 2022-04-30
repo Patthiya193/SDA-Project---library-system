736ba8e37478b41f405e6a9e3a47162ad93dd514
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-30 17:13:36 +0700

    Fix bug

--- RentUrBook/src/components/render/orderItem.js
+++ RentUrBook/src/components/render/orderItem.js
@@ -1,22 +1,25 @@
 
 import React, { useEffect, useState } from "react";
 import { TouchableOpacity, Text, View, Image } from "react-native";
+import { getBookById } from "../../network/bookService";
 import { BookIcon } from "../../screens/Home/bookIcon";
 
 import { bookItemStyles } from "../../screens/universalStyles";
 
-export const OrderItem = ({item, onPress}) => 
-     {
-         const [image, setImage] = useState(null)
+export const OrderItem = ({item, onPress}) => {
+    const [image, setImage] = useState(null)
+
     useEffect(() => {
+        console.log(item.bookId)
         getBookById(item.bookId)
         .then(book => setImage(book["coverImage"])
+        
     ,[])
         
-    })(
+    })
+    return (
         <TouchableOpacity style={bookItemStyles.bookContainer} onPress = {onPress}>
-        { image ? (<Image source={{uri: `data:image/jpeg;base64,${image}`}} style={{width: 100, height: 150, paddingBottom:10}} />) : 
-        (<></>)}
+        { image ? (<Image source={{uri: `data:image/jpeg;base64,${image}`}} style={{width: 100, height: 150, paddingBottom:10}} />) : (<></>)}
         <View style={{justifyContent:'center'}}>
             <Text style={bookItemStyles.bookName}>{item.title}</Text>
             <Text style={bookItemStyles.bookDetails}>Borrowed by: {item.borrowedBy}</Text>
