4fef81ed6bc4d07d0f3fb7164a9c176d3771d9b5
Phurit Warapattanapong <phuritwarapat@gmail.com>
2022-04-30 17:09:17 +0700

    History book image

--- RentUrBook/src/components/render/orderItem.js
+++ RentUrBook/src/components/render/orderItem.js
@@ -1,13 +1,22 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TouchableOpacity, Text, View, Image } from "react-native";
 import { BookIcon } from "../../screens/Home/bookIcon";
 
 import { bookItemStyles } from "../../screens/universalStyles";
 
-export const OrderItem = ({item, onPress}) => (
-    <TouchableOpacity style={bookItemStyles.bookContainer} onPress = {onPress}>
-        {/* <Image source={{uri: `data:image/jpeg;base64,${item.bookObject["coverImage"]}`}} style={{width: 100, height: 150, paddingBottom:10}} /> */}
+export const OrderItem = ({item, onPress}) => 
+     {
+         const [image, setImage] = useState(null)
+    useEffect(() => {
+        getBookById(item.bookId)
+        .then(book => setImage(book["coverImage"])
+    ,[])
+        
+    })(
+        <TouchableOpacity style={bookItemStyles.bookContainer} onPress = {onPress}>
+        { image ? (<Image source={{uri: `data:image/jpeg;base64,${image}`}} style={{width: 100, height: 150, paddingBottom:10}} />) : 
+        (<></>)}
         <View style={{justifyContent:'center'}}>
             <Text style={bookItemStyles.bookName}>{item.title}</Text>
             <Text style={bookItemStyles.bookDetails}>Borrowed by: {item.borrowedBy}</Text>
@@ -17,4 +26,6 @@ export const OrderItem = ({item, onPress}) => (
 
         </View>
     </TouchableOpacity>
-);
+    )
+    
+};
